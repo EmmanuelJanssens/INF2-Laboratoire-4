10cd106263b79ff3371a2c53207d566e067db9ce
Emmanuel Janssens <emmanuel.janssens@heig-vd.ch>
2019-04-08 22:15:57 +0200

    Debut exception

--- Exception.h
+++ Exception.h
@@ -2,13 +2,46 @@
 #define EXCEPTION_H
 
 #include <exception>
-class out: public std::logic_error
+
+std::string makeMessage(std::string file, std::string function , int line)
+{
+
+	std::string message = "error in " + file + " in function " + function + " at line " + std::to_string(line);
+	return message;
+}
+
+class index_error : public std::logic_error
 {
 public:
-
-	explicit out(const std::string& s):  logic_error(s){ }
-	explicit out(const char* c): logic_error(c){}
-
+	explicit index_error(const std::string& s) : logic_error("index error " + s) { }
+	explicit index_error(const char* c) : logic_error(c) {}
 };
+
+class vecteur_length_error : public std::logic_error
+{
+	explicit vecteur_length_error(const std::string& s) : logic_error("length error " + s) { }
+	explicit vecteur_length_error(const char* c) : logic_error(c) {}
+};
+class  vector_length_mismatch : public std::logic_error
+{
+public:
+	explicit vector_length_mismatch(const std::string& s) : logic_error("vector length mismatch " + s) { }
+	explicit vector_length_mismatch(const char* c) : logic_error(c) {}
+};
+
+class not_square_matrix : public std::logic_error
+{
+public:
+	explicit not_square_matrix(const std::string& s) : logic_error("trying to add diagonal of a non square matrix " +s) { }
+	explicit not_square_matrix(const char* c) : logic_error(c) {}
+};
+
+class matrix_size_mismatch : public std::logic_error
+{
+public:
+	explicit matrix_size_mismatch(const std::string& s) : logic_error("matrix size mismatch "+s) { }
+	explicit matrix_size_mismatch(const char* c) : logic_error(c) {}
+};
+
 
 #endif
--- Vecteur_implementation.h
+++ Vecteur_implementation.h
@@ -1,101 +1,133 @@
-
+	
 #ifndef VECTEUR_IMPLEM_H
 #define VECTEUR_IMPLEM_H
 
 #include "Vecteur.h"
 
-
 template <typename T>
-Vecteur<T>::Vecteur(std::size_t n) {
-   _data.resize(n);
+Vecteur<T>::Vecteur(std::size_t n)
+{
+	try
+	{
+		_data.resize(n);
+	}
+	catch (...)
+	{
+
+	}
 }
 
 template <typename T>
-Vecteur<T>::Vecteur(const std::vector<T>& t) {
-   _data.resize(t.size());
-   std::copy(t.begin(), t.end(), _data.begin());
-}
+Vecteur<T>::Vecteur(const std::vector<T>& t)
+{
+	try
+	{
+		_data.resize(t.size());
+		std::copy(t.begin(), t.end(), _data.begin());
+	}
+	catch (...)
+	{
 
-template <typename T>
-std::size_t Vecteur<T>::somme(){
-   std::size_t toReturn = 0;
-   for(std::size_t i = 0; i < this->size(); i++){
-      toReturn += this->at(i);
-   }
-   return toReturn;
-}
-
-template <typename T>
-T& Vecteur<T>::at(std::size_t pos) {
-   if (pos < _data.size())
-      return _data.at(pos);
-   else {
-      //error 
-   }
-}
-
-template <typename T>
-const T& Vecteur<T>::at(std::size_t pos)const {
-   if (pos < _data.size())
-      return _data.at(pos);
-   else {
-      //error 
-   }
+	}
 
 }
 
 template <typename T>
-std::size_t Vecteur<T>::size()const {
-   return _data.size();
+T& Vecteur<T>::at(std::size_t pos)
+{
+	
+	if (pos < _data.size())
+	{
+		return _data.at(pos);
+	}
+    else
+    {			
+		throw index_error(makeMessage(__FILE__,__FUNCTION__,__LINE__));
+    }
+  
+}
+template <typename T>
+const T& Vecteur<T>::at(std::size_t pos)const
+{
+	if (pos < _data.size())
+	{
+	return _data.at(pos);
+	}
+	else
+	{
+		throw index_error(makeMessage(__FILE__, __FUNCTION__, __LINE__));
+	}
+
+}
+template <typename T>
+std::size_t Vecteur<T>::size()const
+{
+	try 
+	{
+		return _data.size();
+	}
+	catch (...)
+	{
+		throw vecteur_length_error(makeMessage(__FILE__, __FUNCTION__, __LINE__));
+	}
 }
 
 template <typename T>
-void Vecteur<T>::resize(std::size_t size) {
-   _data.resize(size, 0);
+void Vecteur<T>::resize(std::size_t size)
+{
+    _data.resize(size,0);
 }
 
 template <typename T>
-Vecteur<T> Vecteur<T>::operator*(const T& valeur) {
-   //check first
-   Vecteur temp(this->size());
-   for (std::size_t i = 0; i < this->_data.size(); i++) {
-      temp.at(i) = this->at(i) * valeur;
-   }
+Vecteur<T> Vecteur<T>::operator*( const T& valeur)
+{
+	Vecteur<T> temp(this->size());
+	//check first
+    for(std::size_t i = 0; i < this->_data.size(); i++)
+    {
+        temp.at(i) = this->at(i) * valeur;
+    }
 
-   return temp;
+	return temp;
 
 }
-
 template <typename T>
-Vecteur<T> Vecteur<T>::operator*(const Vecteur<T>& valeur) {
-   //check first
-   Vecteur temp(this->size());
-   for (std::size_t i = 0; i < this->_data.size(); i++) {
-      temp.at(i) = this->at(i) * valeur.at(i);
-   }
+Vecteur<T> Vecteur<T>::operator*(const Vecteur<T>& valeur)
+{
+	Vecteur<T> temp(this->size());
+	//check first
+	for (std::size_t i = 0; i < this->_data.size(); i++)
+	{
+		temp.at(i) = this->at(i) * valeur.at(i);
+	}
 
-   return temp;
+	return temp;
 }
 
-template <typename T>
-Vecteur<T> Vecteur<T>::operator+(const Vecteur<T>& v) {
-   //check first
-   Vecteur temp(this->size());
-   for (std::size_t i = 0; i < this->_data.size(); i++) {
-      temp.at(i) = this->at(i) + v.at(i);
-   }
 
-   return temp;
+template <typename T>
+Vecteur<T> Vecteur<T>::operator+( const Vecteur<T>& v)
+{
+	//check first
+	//throw sizedifferece("
+	for (std::size_t i = 0; i < this->_data.size(); i++)
+	{
+		this->at(i) = this->at(i) + v.at(i);
+	}
+
+	return *this;
 }
-
 template <typename T>
-Vecteur<T> Vecteur<T>::operator-(const Vecteur<T>& v) {
-   //check first
-   Vecteur temp(this->size());
-   for (std::size_t i = 0; i < this->_data.size(); i++) {
-      temp.at(i) = this->at(i) - v.at(i);
-   }
-   return temp;
+Vecteur<T>& Vecteur<T>::operator-(const Vecteur<T>& v)
+{
+
+	//check first
+	for (std::size_t i = 0; i < this->_data.size(); i++)
+	{
+		this->at(i) = this->at(i) - v.at(i);
+	}
+
+	return *this;
 }
 
 #endif
