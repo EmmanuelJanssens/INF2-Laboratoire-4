125a59c7970b4c7bc7a8de8bba661b9ecad0d8bb
Pierre-O <sandozpierreolivier@yahoo.fr>
2019-04-11 12:20:02 +0200

    changement de la fonction somme et realigné du texte

--- Exception.h
+++ Exception.h
@@ -2,52 +2,78 @@
 #define EXCEPTION_H
 
 #include <exception>
-std::string makeMessage(std::string file, std::string function , int line)
-{
 
-	std::string message = "error in " + file + " in function " + function + " at line " + std::to_string(line);
-	return message;
+std::string makeMessage(std::string file, std::string function, int line) {
+
+   std::string message = "error in " + file + " in function " +
+           function + " at line " + std::to_string(line);
+   return message;
 }
 
-class vecteur_index_error : public std::logic_error
-{
+class vecteur_index_error : public std::logic_error {
 public:
-	explicit vecteur_index_error(const std::string& s) : logic_error("VECTEUR : index error " + s) { }
-	explicit vecteur_index_error(const char* c) : logic_error(c) {}
+
+   explicit vecteur_index_error(const std::string& s) :
+   logic_error("VECTEUR : index error " + s) {
+   }
+
+   explicit vecteur_index_error(const char* c) : logic_error(c) {
+   }
 };
 
-class vecteur_length_error : public std::logic_error
-{
+class vecteur_length_error : public std::logic_error {
 public:
-	explicit vecteur_length_error(const std::string& s) : logic_error("VECTEUR : length error " + s) { }
-	explicit vecteur_length_error(const char* c) : logic_error(c) {}
-};
-class  vector_length_mismatch : public std::logic_error
-{
-public:
-	explicit vector_length_mismatch(const std::string& s) : logic_error("VECTEUR : length mismatch " + s) { }
-	explicit vector_length_mismatch(const char* c) : logic_error(c) {}
+
+   explicit vecteur_length_error(const std::string& s) :
+   logic_error("VECTEUR : length error " + s) {
+   }
+
+   explicit vecteur_length_error(const char* c) : logic_error(c) {
+   }
 };
 
-class not_square_matrix : public std::logic_error
-{
+class vector_length_mismatch : public std::logic_error {
 public:
-	explicit not_square_matrix(const std::string& s) : logic_error("MATRIX : illegal operation on non square matrix " +s) { }
-	explicit not_square_matrix(const char* c) : logic_error(c) {}
+
+   explicit vector_length_mismatch(const std::string& s) :
+   logic_error("VECTEUR : length mismatch " + s) {
+   }
+
+   explicit vector_length_mismatch(const char* c) : logic_error(c) {
+   }
 };
 
-class matrix_size_mismatch : public std::logic_error
-{
+class not_square_matrix : public std::logic_error {
 public:
-	explicit matrix_size_mismatch(const std::string& s) : logic_error("MATRIX : matrix size mismatch "+s) { }
-	explicit matrix_size_mismatch(const char* c) : logic_error(c) {}
+
+   explicit not_square_matrix(const std::string& s) :
+   logic_error("MATRIX : illegal operation on non square matrix " + s) {
+   }
+
+   explicit not_square_matrix(const char* c) : logic_error(c) {
+   }
 };
 
-class vecteur_range_error : public std::runtime_error
-{
+class matrix_size_mismatch : public std::logic_error {
 public:
-	explicit vecteur_range_error(const std::string& s) : runtime_error("VECTEUR : could not resize vecteur " + s) { }
-	explicit vecteur_range_error(const char* c) : runtime_error(c) {}
+
+   explicit matrix_size_mismatch(const std::string& s) :
+   logic_error("MATRIX : matrix size mismatch " + s) {
+   }
+
+   explicit matrix_size_mismatch(const char* c) : logic_error(c) {
+   }
+};
+
+class vecteur_range_error : public std::runtime_error {
+public:
+
+   explicit vecteur_range_error(const std::string& s) :
+   runtime_error("VECTEUR : could not resize vecteur " + s) {
+   }
+
+   explicit vecteur_range_error(const char* c) : runtime_error(c) {
+   }
 };
 
 
--- Vecteur_implementation.h
+++ Vecteur_implementation.h
@@ -1,4 +1,4 @@
-	
+
 #ifndef VECTEUR_IMPLEM_H
 #define VECTEUR_IMPLEM_H
 
@@ -6,158 +6,151 @@
 
 
 //runtime error
+
 template <typename T>
-Vecteur<T>::Vecteur(std::size_t n)
-{
-	try
-	{
-		_data.resize(n);
-	}
-	catch (...)
-	{
-		throw vecteur_range_error(makeMessage(__FILE__, __FUNCTION__, __LINE__));
-	}
+Vecteur<T>::Vecteur(std::size_t n) {
+   try {
+      _data.resize(n);
+   } catch (...) {
+      throw vecteur_range_error(makeMessage
+              (__FILE__, __FUNCTION__, __LINE__));
+   }
 }
 
 //runtime error
+
 template <typename T>
-Vecteur<T>::Vecteur(const std::vector<T>& t)
-{
-	try
-	{
-		_data.resize(t.size());
-		std::copy(t.begin(), t.end(), _data.begin());
-	}
-	catch (...)
-	{
-		throw vecteur_range_error(makeMessage(__FILE__, __FUNCTION__, __LINE__));
-	}
+Vecteur<T>::Vecteur(const std::vector<T>& t) {
+   try {
+      _data.resize(t.size());
+      std::copy(t.begin(), t.end(), _data.begin());
+   } catch (...) {
+      throw vecteur_range_error(makeMessage
+              (__FILE__, __FUNCTION__, __LINE__));
+   }
 }
 
 
 //logic error
+
 template <typename T>
-T& Vecteur<T>::at(std::size_t pos)
-{
-	if (pos < _data.size())
-	{
-		return _data.at(pos);
-	}
-    else
-    {			
-		throw vecteur_index_error(makeMessage(__FILE__,__FUNCTION__,__LINE__) + " : could not acces proper index");
-    }
-  
+T& Vecteur<T>::at(std::size_t pos) {
+   if (pos < _data.size()) {
+      return _data.at(pos);
+   } else {
+      throw vecteur_index_error(makeMessage
+              (__FILE__, __FUNCTION__, __LINE__) +
+              " : could not acces proper index");
+   }
+
 }
 
 //logic error
+
 template <typename T>
-const T& Vecteur<T>::at(std::size_t pos)const
-{
-	if (pos < _data.size())
-	{
-		return _data.at(pos);
-	}
-	else
-	{
-		throw vecteur_index_error(makeMessage(__FILE__, __FUNCTION__, __LINE__) +" : could not acces proper index");
-	}
+const T& Vecteur<T>::at(std::size_t pos)const {
+   if (pos < _data.size()) {
+      return _data.at(pos);
+   } else {
+      throw vecteur_index_error(makeMessage
+              (__FILE__, __FUNCTION__, __LINE__) +
+              " : could not acces proper index");
+   }
 
 }
 
 //No exception
+
 template <typename T>
-std::size_t Vecteur<T>::size()const
-{
-	return _data.size();
+std::size_t Vecteur<T>::size()const {
+   return _data.size();
 }
 
 //runtime error
+
 template <typename T>
-void Vecteur<T>::resize(std::size_t size)
-{
-	try
-	{
-		_data.resize(size, 0);
-	}
-	catch (...)
-	{
-		throw vecteur_range_error(makeMessage(__FILE__, __FUNCTION__, __LINE__) +" : could not resize vector data");
-	}
+void Vecteur<T>::resize(std::size_t size) {
+   try {
+      _data.resize(size, 0);
+   } catch (...) {
+      throw vecteur_range_error(makeMessage
+              (__FILE__, __FUNCTION__, __LINE__) +
+              " : could not resize vector data");
+   }
 }
 
 //numeric error
-template <typename T>
-Vecteur<T> Vecteur<T>::operator*( const T& valeur)
-{
-	Vecteur<T> temp(this->size());
-	//check first
-    for(std::size_t i = 0; i < this->_data.size(); i++)
-    {
-        temp.at(i) = this->at(i) * valeur;
-    }
 
-	return temp;
+template <typename T>
+Vecteur<T> Vecteur<T>::operator*(const T& valeur) {
+   Vecteur<T> temp(this->size());
+   //check first
+   for (std::size_t i = 0; i < this->_data.size(); i++) {
+      temp.at(i) = this->at(i) * valeur;
+   }
+
+   return temp;
 
 }
 
 //numeric error
+
 template <typename T>
-Vecteur<T> Vecteur<T>::operator*(const Vecteur<T>& vecteur)
-{
+Vecteur<T> Vecteur<T>::operator*(const Vecteur<T>& vecteur) {
 
-	if (vecteur.size() != this->size())
-		throw vector_length_mismatch(makeMessage(__FILE__, __FUNCTION__, __LINE__) + " : Trying to multiply vectors of different length");
+   if (vecteur.size() != this->size())
+      throw vector_length_mismatch(makeMessage
+           (__FILE__, __FUNCTION__, __LINE__) +
+           " : Trying to multiply vectors of different length");
 
-	Vecteur<T> temp(this->size());
-	//check first
-	for (std::size_t i = 0; i < this->_data.size(); i++)
-	{
-		temp.at(i) = this->at(i) * vecteur.at(i);
-	}
+   Vecteur<T> temp(this->size());
+   //check first
+   for (std::size_t i = 0; i < this->_data.size(); i++) {
+      temp.at(i) = this->at(i) * vecteur.at(i);
+   }
 
-	return temp;
+   return temp;
 }
 
 //numeric error
-template <typename T>
-Vecteur<T> Vecteur<T>::operator+( const Vecteur<T>& v)
-{
-	if (v.size() != this->size())
-		throw vector_length_mismatch(makeMessage(__FILE__, __FUNCTION__, __LINE__) + " : Trying to add vectors of different length");
-	Vecteur<T> temp(this->size());
-	//check first
-	//throw sizedifferece("
-	for (std::size_t i = 0; i < this->_data.size(); i++)
-	{
-		temp = this->at(i) + v.at(i);
-	}
 
-	return temp;
+template <typename T>
+Vecteur<T> Vecteur<T>::operator+(const Vecteur<T>& v) {
+   if (v.size() != this->size())
+      throw vector_length_mismatch(makeMessage
+           (__FILE__, __FUNCTION__, __LINE__) +
+           " : Trying to add vectors of different length");
+   Vecteur<T> temp(this->size());
+   //check first
+   //throw sizedifferece("
+   for (std::size_t i = 0; i < this->_data.size(); i++) {
+      temp = this->at(i) + v.at(i);
+   }
+
+   return temp;
 }
 
 //numeric error
-template <typename T>
-Vecteur<T>& Vecteur<T>::operator-(const Vecteur<T>& v)
-{
-	Vecteur<T> temp(this->size());
-	//check first
-	for (std::size_t i = 0; i < this->_data.size(); i++)
-	{
-		this->at(i) = this->at(i) - v.at(i);
-	}
 
-	return temp;
+template <typename T>
+Vecteur<T>& Vecteur<T>::operator-(const Vecteur<T>& v) {
+   Vecteur<T> temp(this->size());
+   //check first
+   for (std::size_t i = 0; i < this->_data.size(); i++) {
+      this->at(i) = this->at(i) - v.at(i);
+   }
+
+   return temp;
 }
 
 template <typename T>
-std::size_t Vecteur<T>::somme() 
-{
-	std::size_t toReturn = 0;
-	for (std::size_t i = 0; i < this->size(); i++) 
-	{
-		toReturn += this->at(i);
-	}
-	return toReturn;
+std::size_t Vecteur<T>::somme() {
+   if (this->size()) {
+      T toReturn = this->at(0);
+      for (std::size_t i = 1; i < this->size(); i++) {
+         toReturn += this->at(i);
+      }
+      return toReturn;
+   }
 }
 #endif
